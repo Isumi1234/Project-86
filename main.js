0478027feb280c2e8ee02a6642cd5b6ec99714be
Isumi1234 <86345189+Isumi1234@users.noreply.github.com>
2021-10-05 18:44:00 +1300

    Add files via upload

--- main.js
+++ main.js
@@ -7,11 +7,11 @@ function new_image()
     fabric.Image.fromURL("BirthdayImage.jpg", function(Img) {
         block_image_object=Img;
 
-        block_image_object.scaleToWidth(block_image_width);
-        block_image_object.scaleToHeight(blacok_image_height);
+        block_image_object.scaleToWidth(700);
+        block_image_object.scaleToHeight(510);
         block_image_object.set({
-            top:player_y,
-            left:player_x,
+            top:0,
+            left:0,
          });
         canvas.add(block_image_object);
     })
